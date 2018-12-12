b47ca99c42608d485ea028ed76941cd8f10d33c8
Bruno Lopes <brunoleonelopes@gmail.com>
2018-12-12 11:20:31 +0000

    commented a few lines, added the option for --host and made structural changes to the inventory.go file

--- inventory.go
+++ inventory.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"github.com/mlimaloureiro/ansible-rackhd-inventory/rackhd"
 	"encoding/json"
 	"fmt"
 	"github.com/alexflint/go-arg"
+	"github.com/mlimaloureiro/ansible-rackhd-inventory/rackhd"
 	"github.com/spf13/viper"
 )
 
@@ -15,6 +15,7 @@ type props struct {
 
 type CLIArgs struct {
 	List bool
+	Host string
 }
 
 func main() {
@@ -30,33 +31,42 @@ func main() {
 
 	args := CLIArgs{}
 	arg.MustParse(&args)
+	// If argument "--list" was set to true
 	if args.List {
-		handleList(props)
-		return
+		output ,err := handleList(props)
+		if err != nil {
+			panic(fmt.Errorf("Fatal error handling list: %s \n", err))
+		}
+		marshalResult, _ := json.MarshalIndent(output, "", "  ")
+		fmt.Println(string(marshalResult))
+		// If argument "--host" was set instead of host.
+	} else if args.Host != "" {
+		output, err := handleHost(args.Host, props)
+		if err != nil {
+			panic(fmt.Errorf("Fatal error handling host: %s \n", err))
+		}
+		fmt.Printf("%+v\n", output)
 	}
-
-	handleList(props)
-
-	return
 }
 
-func handleList(props props) {
+// handleList returns a map with tags as keys and the list of hosts as values
+func handleList(props props) (map[string]interface{}, error){
+	// rackhdClient allows us to make calls to the REST API located at BaseUrl
 	rackhdClient := rackhd.Client{BaseUrl: props.rackhdUrl}
 
-	output := make(map[string]interface{})
 	hostvars := Hostvars{}
-
+	// ouput is used to return a map containing the host groups
+	output := make(map[string]interface{})
 	for _, group := range props.groups {
+		// Makes an request to the RackHD api
 		result, err := rackhdClient.GetTaggedNodesIpAddress(group)
 		if err != nil {
-			fmt.Println(err)
-			return
+			return output, err
 		}
-
+		// if there are no host for a given tag we jump to the next iteration
 		if len(result) == 0 {
 			continue
 		}
-
 		groupItem := GroupItem{}
 		for _, item := range result {
 			hostvars[item] = HostvarsItem{
@@ -71,6 +81,12 @@ func handleList(props props) {
 	output["_meta"] = Meta{
 		Hostvars: hostvars,
 	}
-	marshalResult, _ := json.MarshalIndent(output, "", "  ")
-	fmt.Println(string(marshalResult))
+	return output,nil
+}
+
+// TODO: Return hostvars values from this function
+func handleHost(host string, props props) (map[string]interface{}, error){
+	fmt.Println(host)
+	fmt.Printf("%+v\n", props)
+	return nil, nil
 }
