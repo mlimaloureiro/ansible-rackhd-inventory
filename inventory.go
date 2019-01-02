b31baef9c8f50f77c643333916f42d4aee8bbc66
Bruno Lopes <brunoleonelopes@gmail.com>
2019-01-02 17:30:24 +0000

    remove fixtures.go file and put constants in inventory.go file

--- inventory.go
+++ inventory.go
@@ -8,6 +8,21 @@ import (
 	"github.com/spf13/viper"
 )
 
+const (
+	RackHdApiUrlEnvVarName  = "RACK_HD_API_URL"
+	AnsibleRackHdConfigPath = "ANSIBLE_RACKHD_CONFIG_PATH"
+	EmptyString             = ""
+	TwoSpaceString          = "  "
+	EmptyBrackets           = "{}"
+	EmptyBoxBrackets        = `[]`
+	TagCephNode             = "ceph-node"
+	TagCephMon              = "ceph-mon"
+	TagNew                  = "new"
+	lookupPath              = "/api/2.0/lookups"
+	tagsPath                = "/api/current/tags"
+	tagsPathTemplate        = "/api/current/tags/{tag}/nodes"
+)
+
 type props struct {
 	rackhdUrl   string
 	groups      []string
